a8379d620ef0e09ad66cc28140bcefdc244ce2a8
Oscar Gbadou <ogbadou@presidence.bj>
2022-07-28 17:00:01 +0100

    dokku config updated

--- after.sh
+++ after.sh
@@ -2,6 +2,7 @@ chmod -R 775 public
 chmod -R 775 storage
 php artisan storage:link
 php artisan migrate --force
+php artisan package:discover --ansi
 php artisan db:seed --force
 php artisan config:cache
 php artisan route:cache
