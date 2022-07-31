3e964556a2776055c1af298df9c645bf02ef63a2
Oscar Gbadou <ogbadou@presidence.bj>
2022-07-31 08:36:43 +0100

    up

--- after.sh
+++ after.sh
@@ -1,9 +1,9 @@
 chmod -R 775 public
 chmod -R 775 storage
-# php artisan storage:link
-# php artisan migrate --force
-# php artisan package:discover --ansi
-# php artisan db:seed --force
-# php artisan config:cache
-# php artisan route:cache
-# php artisan view:cache
+php artisan storage:link
+php artisan migrate --force
+php artisan package:discover --ansi
+php artisan db:seed --force
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
